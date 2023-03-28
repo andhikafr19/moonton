662828684dd8c552f09f9f3806ddef631d647831
Andhika Ramadhan <andhikafirdaus@gmail.com>
2023-03-28 10:40:24 +0700

    Slicing part 1

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+
+Route::redirect('/', '/prototype/login');
+
+Route::prefix('prototype')->group(function () {
+    Route::get('/login', function () {
+        return Inertia::render('Prototype/Login');
+    });
 });
 
 Route::get('/dashboard', function () {
